2f01d6b5b279150e324028459fc191b77173946c
Steven <666666>
2018-11-22 16:31:27 +0800

    springboot helloworld init

---
+++ src/main/java/com/steven/springboothelloworld/StartApplication.java
@@ -0,0 +1,17 @@
+package com.steven.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class StartApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
---
+++ src/test/java/com/steven/springboothelloworld/StartApplicationTest.java
@@ -0,0 +1,20 @@
+package com.steven.springboothelloworld;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple StartApplication.
+ */
+public class StartApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
